c29dbd8f234954b6eb0448cd91c4d1a173a77307
Exfor <Exfor@MSI>
2024-11-21 17:00:13 +0300

    Proje dosyası ekle.

---
+++ delta/Program.cs
@@ -0,0 +1,29 @@
+﻿double a, b, c;
+Console.WriteLine(" Birinci değeri girin: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci değeri girin: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Üçüncü değeri girin: ");
+c = Convert.ToInt32(Console.ReadLine());
+double delta = b * b - 4 * a * c; ;
+double x1;
+double x2;
+if (delta > 0)
+{
+    x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+    x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+    Console.WriteLine($"KÖK1: {+x1}");
+    Console.WriteLine($"KÖK2: {+x2}");
+
+}
+else if (delta == 0){
+    double x= (-b / (2 * a));
+    Console.WriteLine( $"KÖK: {x}");
+
+}
+else if (delta < 0) Console.WriteLine("Gerçek kök yoktur");
+else Console.WriteLine("Düzgün deger giriniz");
+
+
+
+
